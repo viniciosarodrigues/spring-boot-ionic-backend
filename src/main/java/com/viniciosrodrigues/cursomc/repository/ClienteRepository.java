96e04cb4a14cc1503b6fea5d58ee9550b91e99c3
WPD\vinicios.rodrigues <viniciosarodrigues@gmail.com>
2018-06-30 17:02:07 -0300

    Validação para bloqueio de cadastro para email e CGC já existentes

--- src/main/java/com/viniciosrodrigues/cursomc/repository/ClienteRepository.java
+++ src/main/java/com/viniciosrodrigues/cursomc/repository/ClienteRepository.java
@@ -2,10 +2,17 @@ package com.viniciosrodrigues.cursomc.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.viniciosrodrigues.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+	@Transactional(readOnly = true)
+	public Cliente findByEmail(String email);
+
+	@Transactional(readOnly = true)
+	public Cliente findByCpfOuCnpj(String cpfOuCnpj);
 
 }
--- src/main/java/com/viniciosrodrigues/cursomc/service/validation/ClienteInsertValidator.java
+++ src/main/java/com/viniciosrodrigues/cursomc/service/validation/ClienteInsertValidator.java
@@ -6,12 +6,18 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.viniciosrodrigues.cursomc.domain.enums.TipoCliente;
 import com.viniciosrodrigues.cursomc.dto.ClienteNewDTO;
 import com.viniciosrodrigues.cursomc.exception.FieldMessage;
+import com.viniciosrodrigues.cursomc.repository.ClienteRepository;
 import com.viniciosrodrigues.cursomc.service.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
 
 	@Override
 	public boolean isValid(ClienteNewDTO value, ConstraintValidatorContext context) {
@@ -23,6 +29,14 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
 		if (value.getTipo().equals(TipoCliente.PESSOAJURIDICA.getId()) && !BR.isValidCNPJ(value.getCpfOuCnpj()))
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+
+		if (clienteRepository.findByEmail(value.getEmail()) != null)
+			list.add(new FieldMessage("email", "Email já cadastrado"));
+		if (clienteRepository.findByCpfOuCnpj(value.getCpfOuCnpj()) != null)
+			if (value.getTipo().equals(TipoCliente.PESSOAFISICA.getId()))
+				list.add(new FieldMessage("cpfOuCnpj", "CPF já cadastrado"));
+			else
+				list.add(new FieldMessage("cpfOuCnpj", "CNPJ já cadastrado"));
 
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
