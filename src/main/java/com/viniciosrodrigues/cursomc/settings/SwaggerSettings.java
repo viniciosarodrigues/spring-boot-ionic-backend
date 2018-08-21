171433733b42dc3df27e56816e95a65bdb39f997
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2018-08-20 22:33:05 -0300

    Atualizando documentação com Swagger

--- src/main/java/com/viniciosrodrigues/cursomc/settings/SwaggerSettings.java
+++ src/main/java/com/viniciosrodrigues/cursomc/settings/SwaggerSettings.java
@@ -1,13 +1,17 @@
 package com.viniciosrodrigues.cursomc.settings;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,9 +19,26 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerSettings {
+	private final ResponseMessage m201 = simpleMessage(201, "Recurso criado");
+	private final ResponseMessage m204put = simpleMessage(204, "Atualização ok");
+	private final ResponseMessage m204del = simpleMessage(204, "Deleção ok");
+	private final ResponseMessage m403 = simpleMessage(403, "Não autorizado");
+	private final ResponseMessage m404 = simpleMessage(404, "Não encontrado");
+	private final ResponseMessage m422 = simpleMessage(422, "Erro de validação");
+	private final ResponseMessage m500 = simpleMessage(500, "Erro inesperado");
+
+	private ResponseMessage simpleMessage(int code, String msg) {
+		return new ResponseMessageBuilder().code(code).message(msg).build();
+	}
+
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+		return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false)
+				.globalResponseMessage(RequestMethod.GET, Arrays.asList(m403, m404, m500))
+				.globalResponseMessage(RequestMethod.POST, Arrays.asList(m201, m403, m422, m500))
+				.globalResponseMessage(RequestMethod.PUT, Arrays.asList(m204put, m403, m404, m422, m500))
+				.globalResponseMessage(RequestMethod.DELETE, Arrays.asList(m204del, m403, m404, m500)).select()
+				.apis(RequestHandlerSelectors.basePackage("com.viniciosrodrigues.cursomc.resource"))
 				.paths(PathSelectors.any()).build().apiInfo(apiInfo());
 	}
 
